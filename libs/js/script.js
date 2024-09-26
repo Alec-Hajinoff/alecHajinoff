69fb3d6662153509e0aea252de3dadc5ff2f893f
Alec Hajinoff <hajinoff@gmail.com>
2024-09-26 06:36:26 +0100

    26.9.24, 6:36

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,32 +1,31 @@
-	$('#btnRun').click(function() {
+	$('#btnSubmitOne').click(function() {
 
 		$.ajax({
 			url: "libs/php/getCountryInfo.php",
 			type: 'POST',
 			dataType: 'json',
 			data: {
-				country: $('#selCountry').val(),
-				lang: $('#selLanguage').val()
+				north: $('#paramEarthquakes').val(),
+				south: $('#paramEarthquakes').val(),
+				east: $('#paramEarthquakes').val(),
+				west: $('#paramEarthquakes').val()
 			},
+			
 			success: function(result) {
 
 				console.log(JSON.stringify(result));
 
 				if (result.status.name == "ok") {
 
-					$('#txtContinent').html(result['data'][0]['continent']);
-					$('#txtCapital').html(result['data'][0]['capital']);
-					$('#txtLanguages').html(result['data'][0]['languages']);
-					$('#txtPopulation').html(result['data'][0]['population']);
-					$('#txtArea').html(result['data'][0]['areaInSqKm']);
+					$('#txtResults').html(JSON.stringify(result['data']));
 
 				}
 			
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code here
+				console.log("This enquiry did not produce results:-(");
 			}
-		}); 
+		});
 	
 	});
 	/*
