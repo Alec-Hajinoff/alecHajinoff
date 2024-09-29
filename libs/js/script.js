aca4813ec69af756ee629c2e6996ac2b4a6bf971
Alec Hajinoff <hajinoff@gmail.com>
2024-09-29 15:40:13 +0100

    29.09.24, first commit

--- libs/js/script.js
+++ libs/js/script.js
@@ -28,6 +28,7 @@
 		});
 	
 	});
+
 	$('#btnSubmitTwo').click(function() {
 
 		$.ajax({
@@ -37,6 +38,37 @@
 			data: {
 				lat: $('#paramLat').val(),
 				lng: $('#paramLng').val()
+			},
+			
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#txtResults').html(JSON.stringify(result['data']));
+
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("This enquiry did not produce results:-(");
+			}
+		});
+	
+	});
+
+	$('#btnSubmitThree').click(function() {
+
+		$.ajax({
+			url: "libs\php\getCountryInfoWeather.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				north: $('#paramNorthWeather').val(),
+				south: $('#paramSouthWeather').val(),
+				east: $('#paramEastWeather').val(),
+				west: $('#paramWestWeather').val()
 			},
 			
 			success: function(result) {
