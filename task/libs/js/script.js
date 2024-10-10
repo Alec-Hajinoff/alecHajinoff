319190df8f0310c7152e2133b1dd77ff16b19366
Alec Hajinoff <hajinoff@gmail.com>
2024-10-10 06:16:46 +0100

    10.10.24, 6.16

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -19,9 +19,9 @@
 
 				if (result.status.name == "ok") {
 					
-					$('#txtResults').html(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
+				$('#txtResults').html(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
 					
-					//$('#txtResults').html(JSON.stringify(result['data']));
+				//$('#txtResults').html(JSON.stringify(result['data']));
 
 				}
 			
@@ -76,11 +76,11 @@
 			
 			success: function(result) {
 	
-				console.log(JSON.stringify(result));
+				console.log(JSON.stringify(result['data'][0]["lng"] + " || " + result['data'][0]["observation"] + " || " + result['data'][0]["ICAO"] + " || " + result['data'][0]["clouds"] + " || " + result['data'][0]["dewPoint"] + " || " + result['data'][0]["datetime"] + " || " + result['data'][0]["temperature"] + " || " + result['data'][0]["humidity"] + " || " + result['data'][0]["stationName"] + " || " + result['data'][0]["weatherCondition"] + " || " + result['data'][0]["windDirection"] + " || " + result['data'][0]["windSpeed"] + " || " + result['data'][0]["lat"]));
 	
 				if (result.status.name == "ok") {
 	
-					$('#txtResults').html(JSON.stringify(result['data']));
+				$('#txtResults').html(JSON.stringify(result['data'][0]["lng"] + " || " + result['data'][0]["observation"] + " || " + result['data'][0]["ICAO"] + " || " + result['data'][0]["clouds"] + " || " + result['data'][0]["dewPoint"] + " || " + result['data'][0]["datetime"] + " || " + result['data'][0]["temperature"] + " || " + result['data'][0]["humidity"] + " || " + result['data'][0]["stationName"] + " || " + result['data'][0]["weatherCondition"] + " || " + result['data'][0]["windDirection"] + " || " + result['data'][0]["windSpeed"] + " || " + result['data'][0]["lat"]));
 	
 				}
 			
