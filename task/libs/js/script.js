3b99a091da919e43e8771dd15c0e8c2f51fd86e2
Alec Hajinoff <hajinoff@gmail.com>
2024-10-11 14:01:08 +0100

    11.10.24, 14.01

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -14,14 +14,10 @@
 			success: function(result) {
 
 				console.log(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
-				
-				//console.log(JSON.stringify(result));
 
 				if (result.status.name == "ok") {
 					
 				$('#txtResults').html(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
-					
-				//$('#txtResults').html(JSON.stringify(result['data']));
 
 				}
 			
@@ -46,11 +42,11 @@
 			
 			success: function(result) {
 
-				console.log(JSON.stringify(result));
+				console.log(JSON.stringify(result['data']['distance'] + " || " + result['data']['geonameId'] + " || " + result['data']['name']));
 
 				if (result.status.name == "ok") {
 
-					$('#txtResults').html(JSON.stringify(result['data']));
+				$('#txtResults').html(JSON.stringify(result['data']['distance'] + " || " + result['data']['geonameId'] + " || " + result['data']['name']));
 
 				}
 			
