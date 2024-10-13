976e54d516a69e03d121fd7ac892e9b905bdbd1a
Alec Hajinoff <hajinoff@gmail.com>
2024-10-13 14:56:23 +0100

    13.10.24, 14.00

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,2 +1,4 @@
-let d = new Date();
-let b = document.getElementById("one");
+let d = new Date()
+var pElement = document.getElementById("one");
+var content = pElement.innerHTML;
+pElement.innerHTML = d;
