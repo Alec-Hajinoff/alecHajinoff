f830a3982d44e272d1e637dede4670964d705baa
Alec Hajinoff <hajinoff@gmail.com>
2024-10-16 06:36:32 +0100

    16.10.24, 5.02

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,2 +1,18 @@
 let d = new Date()
 document.getElementById("one").innerHTML = d;
+var two = document.getElementById("CountryName");
+var msg = "Sorry, we couldn't get your location.";
+if (Modernizr.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, fail);
+    two.textContent = "Checking location..";
+} else {
+    two.textContent = msg;
+}
+function success(position) {
+    msg = "Longitude: " + pos.coords.longitude + "Latitude: " + pos.coords.lantitude;
+    two.innerHTML = msg;
+}
+function fail(msg) {
+    two.textContent = msg;
+    console.log(msg.code);
+}
