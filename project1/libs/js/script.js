e6313edfb94f5385f0a10ad2e07b258303494e0d
Alec Hajinoff <hajinoff@gmail.com>
2024-10-17 06:15:47 +0100

    17.10.24, 6.00

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -10,3 +10,25 @@ function fail() {
     msg = "Sorry, we couldn't get your location!";
     two.innerHTML = msg;
 }
+$('#btnSubmitOne').click(function() {
+    $.ajax({
+        url: "libs/php/getCountryInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#paramNorth').val(),
+            south: $('#paramSouth').val(),
+            east: $('#paramEast').val(),
+            west: $('#paramWest').val()
+        },
+        success: function(result) {
+            console.log(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
+            if (result.status.name == "ok") {  
+            $('#txtResults').html(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("This enquiry did not produce results:-(");
+        }
+    });
+});
