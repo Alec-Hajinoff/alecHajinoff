6269eba57c26df9127fab15b201bec27b2a85512
Alec Hajinoff <hajinoff@gmail.com>
2024-10-18 13:43:21 +0100

    18.10.24, 13.00

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -5,18 +5,16 @@ navigator.geolocation.getCurrentPosition(success, fail);
 function success(pos) {
     $.ajax({
         url: "libs/php/getCountryMap.php",
-        type: 'POST',
+        type: 'GET',
         dataType: 'json',
-        results: {
-            north: $('#paramNorth').val(),
-            south: $('#paramSouth').val(),
-            east: $('#paramEast').val(),
-            west: $('#paramWest').val()
+        data: {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
         },
         success: function(result) {
-            console.log(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
+            console.log(JSON.stringify(result['data']));
             if (result.status.name == "ok") {  
-            $('#txtResults').html(JSON.stringify(result['data'][0]["datetime"] + " || " + result['data'][0]["depth"] + " || " + result['data'][0]["lng"] + " || " + result['data'][0]["src"] + " || " + result['data'][0]["eqid"] + " || " + result['data'][0]["magnitude"] + " || " + result['data'][0]["lat"]));
+            $('#CountryName').html(JSON.stringify(result['data']));
             }
         },
         error: function(jqXHR, textStatus, errorThrown) {
--- project1/libs/php/getCountryMap.php
+++ project1/libs/php/getCountryMap.php
@@ -2,7 +2,7 @@
     ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 	$executionStartTime = microtime(true);
-	$url='http://api.geonames.org/earthquakesJSON?north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] . '&east=' . $_REQUEST['east'] . '&west=' . $_REQUEST['west'] . '&username=hertford_networks';
+	$url='https://api.opencagedata.com/geocode/v1/json?q=' . $_REQUEST['latitude'] . '%2C' . $_REQUEST['longitude'] . '&key=a7f882d3ba784ed39e3ceed781eb6b3c';
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -14,7 +14,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['earthquakes'];
+	$output['data'] = $decode['geocode'];
 	header('Content-Type: application/json; charset=UTF-8');
 	echo json_encode($output);
 ?>
