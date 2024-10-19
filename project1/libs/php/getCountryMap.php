76e54efc4c6b9a3e9fb8a44a57f348142e21c9cf
Alec Hajinoff <hajinoff@gmail.com>
2024-10-19 17:58:34 +0100

    19.10.24, 17.01

--- project1/libs/php/getCountryMap.php
+++ project1/libs/php/getCountryMap.php
@@ -14,7 +14,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geocode'];
+	$output['data'] = $decode['results'];
 	header('Content-Type: application/json; charset=UTF-8');
 	echo json_encode($output);
 ?>
